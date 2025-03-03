ffb379cc50104017f840dbc7173414c15562404e
Kajol Gehlot <kajolgehlot980@gmail.com>
2025-03-03 15:58:41 +0530

    Added new Scree

---
+++ adv_basics/lib/questions_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  Widget build(context) {
+    return const Text("Hello world");
+  }
+}
